300e397047088bbb6f588bf8c48d604df61b7eab
victespinoza <tutosudemy@gmail.com>
2019-03-31 11:48:42 -0300

    Conexion con react dev tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 
 import reducers from './reducers';
 import App from './components/App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App/>
     </Provider>
     ,
